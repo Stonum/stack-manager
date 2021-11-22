0469664ec46b18c83bab6faddcf791faa657ccd9
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2021-11-22 14:21:36 +0300

    перенес бэк в отдельный каталог

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,32 +1,64 @@
-import "@mdi/font/css/materialdesignicons.css";
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
-import ru from "vuetify/src/locale/ru";
+import '@mdi/font/css/materialdesignicons.css';
+
+import Vue from 'vue';
+
+import Vuetify from 'vuetify/lib/framework';
+
+import ru from 'vuetify/src/locale/ru';
+
+
 
 Vue.use(Vuetify);
 
+
+
 export default new Vuetify({
+
   theme: {
+
     options: {
+
       customProperties: true,
+
     },
+
     themes: {
+
       light: {
-        primary: "#007BFF",
-        secondary: "#424242",
-        accent: "#82B1FF",
-        error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107",
+
+        primary: '#007BFF',
+
+        secondary: '#424242',
+
+        accent: '#82B1FF',
+
+        error: '#FF5252',
+
+        info: '#2196F3',
+
+        success: '#4CAF50',
+
+        warning: '#FFC107',
+
       },
+
     },
+
   },
+
   lang: {
+
     locales: { ru },
-    current: "ru",
+
+    current: 'ru',
+
   },
+
   icons: {
-    iconfont: "mdiSvg", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+
+    iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+
   },
+
 });
+
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
+      // Use this to change the entrypoint of your app's main process
+      mainProcessFile: 'src/background/index.ts',
+      // Use this to change the entry point of your app's render process. default src/[main|index].[js|ts]
+      rendererProcessFile: 'src/main.ts',
+      // Provide an array of files that, when changed, will recompile the main process and restart Electron
+      // Your main process file will be added by default
+      // mainProcessWatch: ['src/myFile1', 'src/myFile2'],
     },
   },
 };
