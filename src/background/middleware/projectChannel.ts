9da4781a4326ee0208f75b0504be18534e885d1f
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-02-06 16:36:54 +0300

    chore: ts linter

--- src/background/middleware/projectChannel.ts
+++ src/background/middleware/projectChannel.ts
@@ -295,7 +295,7 @@ async function addProject(payload: Project) {
   }
 
   // проверим валидность путей
-  for (const key of Object.keys(project.path)) {
+  for (const key of Object.keys(project.path) as Array<keyof typeof project.path>) {
     const cpath = project.path[key];
     if (cpath && !fs.existsSync(cpath)) {
       throw new Error(`Некорректный путь ${cpath}`);
