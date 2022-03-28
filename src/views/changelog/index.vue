b3670615f1eab661236adf55b275223f804778b5
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-03-28 14:38:41 +0300

    fix: открытие внешних линков списка изменений во внешнем браузере

--- src/views/changelog/index.vue
+++ src/views/changelog/index.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container fluid>
     <app-bar />
-    <div v-html="html" />
+    <div v-html="html" @click.prevent.stop="onClick" />
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapActions } from 'vuex';
 
 export default Vue.extend({
   name: 'ChangeLog',
@@ -14,6 +15,15 @@ export default Vue.extend({
     return {
       html: null,
     };
+  },
+  methods: {
+    ...mapActions('projectStore', ['projectSendJob']),
+
+    onClick(payload: any) {
+      if (payload.target.href) {
+        this.projectSendJob({ jobName: 'openUrl', projectId: null, params: payload.target.href });
+      }
+    },
   },
   async mounted() {
     this.html = await this.$store.dispatch('mainStore/getChangeLog');
