b07d3b54f3ad122928b254d43e257d5fab620f09
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-04-23 18:33:23 +0300

    chore: список активностей проекта вынес в меню

--- src/views/main/ProjectItem.vue
+++ src/views/main/ProjectItem.vue
@@ -2,6 +2,8 @@
   <v-expansion-panel>
     <v-expansion-panel-header style="padding: 0">
       <v-app-bar flat color="rgba(0, 0, 0, 0)">
+        <v-progress-circular class="mr-3" :size="20" :width="isRunning ? 2 : 0" color="primary" :indeterminate="isRunning" />
+
         <v-toolbar-title>
           {{ item.name }} <a v-if="item.port" class="text-subtitle-1 px-2" :href="projectUrl" @click.stop="onOpenUrl">{{ projectUrl }}</a>
         </v-toolbar-title>
@@ -10,21 +12,24 @@
 
         <v-icon v-for="(app, idxtask) in item.apps" :key="idxtask" small :color="appColor(app.name)"> mdi-circle </v-icon>
 
-        <v-btn icon tile small class="ml-5" title="Git pull" @click.stop="onGitPull" :loading="gitpulling">
-          <v-icon color="accent"> mdi-briefcase-download </v-icon>
-        </v-btn>
-        <v-btn icon tile small title="Собрать front" @click.stop="onBuildFront" :loading="buildingFront">
-          <v-icon color="accent"> mdi-code-tags-check </v-icon>
-        </v-btn>
-        <v-btn icon tile small title="Перезапустить все приложения" @click.stop="onRestart">
-          <v-icon color="primary"> mdi-restart </v-icon>
-        </v-btn>
-        <v-btn icon tile small @click.stop="$emit('edit')">
-          <v-icon color="primary"> mdi-pencil </v-icon>
-        </v-btn>
-        <v-btn icon tile small @click.stop="$emit('delete')">
-          <v-icon color="error"> mdi-delete </v-icon>
-        </v-btn>
+        <v-menu bottom left offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="(item, i) in projectActions" :key="i" @click="item.method">
+              <v-list-item-action>
+                <v-icon :color="item.color">{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-app-bar>
     </v-expansion-panel-header>
 
@@ -52,13 +57,16 @@ export default Vue.extend({
   },
   data() {
     return {
-      buildingFront: false,
-      gitpulling: false,
+      projectActions: [] as any[],
+      runningActions: [] as boolean[],
     };
   },
   computed: {
     projectUrl() {
       return `http://localhost:${this.item.port}`;
+    },
+    isRunning(): boolean {
+      return this.runningActions.some((running: boolean) => running);
     },
   },
   methods: {
@@ -74,31 +82,36 @@ export default Vue.extend({
       this.getAppStatus();
     },
     async onRestart(appname?: string) {
-      if (appname) {
-        await this.projectSendJob({ jobName: 'appReStart', projectId: this.id, params: appname });
-      } else {
-        for (const app of this.item.apps) {
-          await this.projectSendJob({ jobName: 'appReStart', projectId: this.id, params: app.name });
+      const idx = this.runningActions.push(true) - 1;
+      try {
+        if (appname && typeof appname === 'string') {
+          await this.projectSendJob({ jobName: 'appReStart', projectId: this.id, params: appname });
+        } else {
+          for (const app of this.item.apps) {
+            await this.projectSendJob({ jobName: 'appReStart', projectId: this.id, params: app.name });
+          }
         }
+      } finally {
+        this.$set(this.runningActions, idx, false);
+        this.getAppStatus();
       }
-      this.getAppStatus();
     },
 
     async onBuildFront() {
-      this.buildingFront = true;
+      const idx = this.runningActions.push(true) - 1;
       try {
         await this.projectSendJob({ jobName: 'buildFront', projectId: this.id });
       } finally {
-        this.buildingFront = false;
+        this.$set(this.runningActions, idx, false);
       }
     },
 
     async onGitPull() {
-      this.gitpulling = true;
+      const idx = this.runningActions.push(true) - 1;
       try {
         await this.projectSendJob({ jobName: 'gitPull', projectId: this.id });
       } finally {
-        this.gitpulling = false;
+        this.$set(this.runningActions, idx, false);
       }
     },
 
@@ -110,6 +123,27 @@ export default Vue.extend({
     appColor(name: string | undefined) {
       return this.$store.getters['projectStore/getAppColor'](name);
     },
+  },
+  mounted() {
+    this.projectActions.push({ name: 'Обновить гит', icon: 'mdi-briefcase-download', color: 'accent', method: this.onGitPull });
+    this.projectActions.push({ name: 'Собрать фронт', icon: 'mdi-code-tags-check', color: 'accent', method: this.onBuildFront });
+    this.projectActions.push({ name: 'Перезапустить', icon: 'mdi-restart', color: 'primary', method: this.onRestart });
+    this.projectActions.push({
+      name: 'Редактировать',
+      icon: 'mdi-pencil',
+      color: 'primary',
+      method: () => {
+        this.$emit('edit');
+      },
+    });
+    this.projectActions.push({
+      name: 'Удалить',
+      icon: 'mdi-delete',
+      color: 'error',
+      method: () => {
+        this.$emit('delete');
+      },
+    });
   },
 });
 </script>
