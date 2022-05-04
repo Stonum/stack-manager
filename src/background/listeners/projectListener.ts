c8fbd29a1b9d26caa11b3143f717ea05baf542f3
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-05-04 13:40:23 +0300

    chore: поправил сборку фронта при первом запуске

--- src/background/listeners/projectListener.ts
+++ src/background/listeners/projectListener.ts
@@ -62,7 +62,7 @@ export class ProjectListener extends CommonListener {
     statuses.push(
       ...apps.map((app: any) => {
         return { name: app.Name, status: +app.State };
-      }),
+      })
     );
 
     const appServer = getDispatcher().appServer();
@@ -70,7 +70,7 @@ export class ProjectListener extends CommonListener {
     statuses.push(
       ...apps.map((app: any) => {
         return { name: app.Name, status: +app.State ? 0 : 2 };
-      }),
+      })
     );
 
     return statuses;
@@ -237,7 +237,11 @@ export class ProjectListener extends CommonListener {
       }
 
       this.sendInfoMessage(project.name, 'Сборка запущена');
-      await cmd.exec('npm ci', project.path.front);
+      if (fs.existsSync(path.join(project.path.front, 'node_modules'))) {
+        await cmd.exec('npm ci --progress=false', project.path.front);
+      } else {
+        await cmd.exec('npm install --progress=false', project.path.front);
+      }
       await cmd.exec('npm run build', project.path.front);
       if (fs.existsSync(path.join(project.path.front, 'dist'))) {
         if (fs.existsSync(path.join(this.staticPath, project.name))) {
@@ -981,7 +985,7 @@ async function generateGatewaySettings(project: Project, pathnew: string) {
             useAsyncCache: false,
           },
         ];
-      }),
+      })
     );
 
     common.stack.queue.service.exchangeIn = os.hostname + '_' + project.name + '_service_from_backend';
