238994864cca12db68d77254969771fc9395f3d2
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-05-18 13:12:30 +0300

    fix: добавил проверку наличия application.yml

--- src/background/listeners/projectListener.ts
+++ src/background/listeners/projectListener.ts
@@ -62,7 +62,7 @@ export class ProjectListener extends CommonListener {
     statuses.push(
       ...apps.map((app: any) => {
         return { name: app.Name, status: +app.State };
-      }),
+      })
     );
 
     const appServer = getDispatcher().appServer();
@@ -70,7 +70,7 @@ export class ProjectListener extends CommonListener {
     statuses.push(
       ...apps.map((app: any) => {
         return { name: app.Name, status: +app.State ? 0 : 2 };
-      }),
+      })
     );
 
     return statuses;
@@ -605,6 +605,10 @@ function checkProject(project: Project, index: number | null) {
       throw new Error(`Некорректный путь ${project.gateway.path}`);
     }
 
+    if (project.gateway.settings && !fs.existsSync(project.gateway.settings)) {
+      throw new Error(`Некорректный путь ${project.gateway.settings}`);
+    }
+
     if (project.gateway.path && !settings.get('jre')) {
       throw new Error(`Не задан каталг jre в настройках`);
     }
@@ -992,7 +996,7 @@ async function generateGatewaySettings(project: Project, pathnew: string) {
             useAsyncCache: false,
           },
         ];
-      }),
+      })
     );
 
     common.stack.queue.service.exchangeIn = os.hostname + '_' + project.name + '_service_from_backend';
