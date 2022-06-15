3fa9cf335e4e9e33d0cf869fa1793ee535d15f43
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-06-15 17:43:29 +0300

    chore: кнопку закрытия диалога сообщения перенес вверх

--- src/components/MessageList/MessageDialog.vue
+++ src/components/MessageList/MessageDialog.vue
@@ -1,18 +1,17 @@
 <template>
-  <!-- <v-row justify="center"> -->
   <v-dialog v-model="value" max-width="80%">
     <v-card>
       <v-card-title :class="msgColor">
         {{ item.type.substring(0, 1).toUpperCase() + item.type.substring(1).toLowerCase() }}
+
+        <v-spacer></v-spacer>
+        <v-btn icon @click="$emit('change', false)">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
       </v-card-title>
       <v-card-text style="white-space: pre-line"> {{ item.text }} </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" text @click="$emit('change', false)"> Ок </v-btn>
-      </v-card-actions>
     </v-card>
   </v-dialog>
-  <!-- </v-row> -->
 </template>
 
 <script lang="ts">
