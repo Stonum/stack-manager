ec1c1109b3d0a60b3e430d424302c47f8c79c45b
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-08-24 17:06:44 +0300

    chore: переделал task tab на v-table

--- packages/renderer/src/views/settings/TasksTab.vue
+++ packages/renderer/src/views/settings/TasksTab.vue
@@ -1,33 +1,31 @@
 <template>
   <v-container fluid>
-    <v-row no-gutters>
-      <v-col cols="4">
-        Название
-      </v-col>
-      <!-- <v-col cols="1">
-        Порт
-      </v-col> -->
-      <v-col cols="1">
-        Префикс
-      </v-col>
-    </v-row>
-    <v-row v-for="task in props.tasks" :key="task.id" dense>
-      <v-col cols="4">
-        <base-checkbox :key="task.id" v-model="task.selected" :label="task.title" hide-details density="compact" />
-      </v-col>
-      <!-- <v-col cols="1">
-        <base-input v-model="task.port" type="number" hide-details density="compact" />
-      </v-col> -->
-      <v-col cols="1">
-        <base-input v-model="task.prefix" hide-details density="compact" />
-      </v-col>
-    </v-row>
+    <v-table density="compact">
+      <thead>
+        <th class="text-left">
+          <span>Название</span>
+        </th>
+        <th class="text-left">
+          <span>Префикс</span>
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks" :key="task.id">
+          <td class="pl-0">
+            <base-checkbox :key="task.id" v-model="task.selected" :label="task.title" hide-details density="compact" />
+          </td>
+          <td class="pl-0">
+            <base-input v-model="task.prefix" hide-details density="compact" />
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
   </v-container>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
-   tasks?: Task[]
+defineProps<{
+  tasks?: Task[];
 }>();
 
 defineEmits(['update:tasks']);
