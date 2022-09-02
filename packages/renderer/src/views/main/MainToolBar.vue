c80e1c19e1fe8bb5560a1682111bac90a91ea098
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-09-02 17:08:56 +0300

    chore: обновление состояний по кнопке

--- packages/renderer/src/views/main/MainToolBar.vue
+++ packages/renderer/src/views/main/MainToolBar.vue
@@ -1,18 +1,21 @@
 <template>
   <app-bar title="Главная">
-    <v-btn title="Перезапустить диспетчер" :loading="isLoading" @click="execute()"> Перезапустить диспетчер </v-btn>
-    <v-btn icon="mdi-refresh" title="Обновить состояния" @click="emit('refresh')" />
+    <v-btn title="Перезапустить диспетчер" :loading="isLoading" @click="execute()">
+      Перезапустить диспетчер
+    </v-btn>
+    <v-btn icon="mdi-refresh" title="Обновить состояния" @click="loadStatuses()" />
     <v-btn icon="mdi-help-circle-outline" title="Список изменений" to="/changelog" />
   </app-bar>
 </template>
 
 <script lang="ts" setup>
 import { whenever } from '@vueuse/shared';
-import { useIpcRendererInvokeAsync } from '@/composables/useIpcRendererInvokeAsync';
-
-const emit = defineEmits(['refresh']);
+import { useIpcRendererInvokeAsync, useApp, useEvents } from '@/composables';
 
 const { isReady, isLoading, execute } = useIpcRendererInvokeAsync('main', { message: 'restartDispatcher' }, false, { immediate: false });
 
-whenever(isReady, () => emit('refresh'));
+const { loadStatuses } = useApp();
+const { loadEvents } = useEvents();
+
+whenever(isReady, () => { loadEvents(); loadStatuses(); } );
 </script>
