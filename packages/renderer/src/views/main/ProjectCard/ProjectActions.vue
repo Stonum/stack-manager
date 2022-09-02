25ed2e8d7d694c228c39fe792298da27e5ed17ee
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2022-09-02 13:14:03 +0300

    style: action project

--- packages/renderer/src/views/main/ProjectCard/ProjectActions.vue
+++ packages/renderer/src/views/main/ProjectCard/ProjectActions.vue
@@ -1,14 +1,24 @@
 <template>
   <v-row class="px-2" justify="space-between" no-gutters>
-    <v-col cols="5" width="20px">
-      <v-btn icon="mdi-web" color="primary" variant="text" :title="projectUrl" :disabled="!props.item.port" :href="projectUrl" />
-      <v-btn icon="mdi-microsoft-visual-studio-code" color="blue" variant="text" title="Workspace" @click="emit('run', 'openWorkspace')" />
+    <v-col cols="5">
+      <v-btn variant="text" icon :title="projectUrl" :disabled="!props.item.port" :href="projectUrl">
+        <v-icon icon="mdi-web" color="primary" size="large" />
+      </v-btn>
+      <v-btn variant="text" title="Workspace" icon @click="emit('run', 'openWorkspace')">
+        <v-icon icon="mdi-microsoft-visual-studio-code" color="blue" size="large" />
+      </v-btn>
     </v-col>
 
     <v-col cols="7" class="text-right">
-      <v-btn icon="mdi-briefcase-download" color="accent" variant="text" title="Обновить гит" :loading="state.pulling" @click="emit('run', 'gitPull')" />
-      <v-btn icon="mdi-code-tags-check" color="accent" variant="text" title="Собрать фронт" :loading="state.deploying" @click="emit('run', 'buildFront')" />
-      <v-btn icon="mdi-restart" color="primary" variant="text" title="Перезапустить" @click="emit('run', 'appReStart')" />
+      <v-btn icon variant="text" title="Обновить гит" :loading="state.pulling" @click="emit('run', 'gitPull')">
+        <v-icon icon="mdi-briefcase-download" color="accent" size="large" />
+      </v-btn>
+      <v-btn icon variant="text" title="Собрать фронт" :loading="state.deploying" @click="emit('run', 'buildFront')">
+        <v-icon icon="mdi-code-tags-check" color="accent" size="large" />
+      </v-btn>
+      <v-btn icon variant="text" title="Перезапустить" @click="emit('run', 'appReStart')">
+        <v-icon icon="mdi-restart" color="primary" size="large" />
+      </v-btn>
     </v-col>
   </v-row>
 </template>
